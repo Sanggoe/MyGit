9c48853b300414f8144719956660677596094c00
sanggoe <sanggoe0509@gmail.com>
2022-06-14 15:45:12 +0900

    Initial commit

---
+++ src/com/company/A.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class A {
+
+    public static void main(String[] args) {
+        B b = new B();
+        C c = new C();
+
+    }
+
+    void sayHello() {
+
+    }
+}
